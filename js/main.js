a621226a65fa25866213c1b52fe6294cd355b9be
Marcos Soares <31077979+marcosandresoares@users.noreply.github.com>
2019-07-01 18:06:01 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    const app = document.querySelector('#app');
+    const disc = document.querySelector('color-disc');
+    const output = document.querySelector('#color');
+    const format = document.querySelector('#format');
+
+
+
+    function updateColor() {
+        output.innerText = disc.getAttribute('color');
+        app.style.backgroundColor = disc.getAttribute('color');
+    }
+
+    function observeColor() {
+        updateColor();
+        const config = {
+            attributes: true
+        };
+        const callback = function(list, observer) {
+            list.forEach(mutation => {
+                if (mutation.attributeName === 'color') {
+                    updateColor();
+                }
+            });
+        };
+        const observer = new MutationObserver(callback);
+        observer.observe(disc, config);
+    }
+
+    function setColorFormat() {
+        format.addEventListener('change', e => {
+            disc.setAttribute('format', e.target.value);
+        });
+    }
+
+    observeColor();
+    setColorFormat();
+
+})
